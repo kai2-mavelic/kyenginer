2d28027d2557370c4834d08ada5e3dbc7d8534db
kylikethestar <heroisgod.heroisme1@gmail.com>
2026-01-03 14:57:02 +0800

    upload UploaderFile/system/helper.js

---
+++ UploaderFile/system/helper.js
@@ -0,0 +1,10 @@
+// numpang lewat
+
+export * from './helper/general-helper.js'
+export * from './helper/baileys-related.js'
+export * from './helper/baileys-send.js'
+
+export * from './plugin-help-serialize.js'
+export * from './all-path.js'
+export * from './bot-info.js'
+export * from '../index.js'
